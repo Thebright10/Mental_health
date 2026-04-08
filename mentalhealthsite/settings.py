158d89484c7b367badcc3a384d4903a09edce8a7
Thebright10 <adaksudip956@gmail.com>
2026-04-08 06:49:40 +0530

    fix allowed hosts

--- mentalhealthsite/settings.py
+++ mentalhealthsite/settings.py
@@ -8,7 +8,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get("SECRET_KEY")
 DEBUG = os.environ.get("DEBUG", "False") == "True"
 
-ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS", "").split(",")
+import os
+
+ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS", "*").split(",")
 
 # 🔧 APPS
 INSTALLED_APPS = [
